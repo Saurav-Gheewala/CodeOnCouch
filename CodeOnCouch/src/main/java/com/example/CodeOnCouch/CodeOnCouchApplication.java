c61df102a609270b35942de886d7afdab561e4e2
Saurav Gheewala <gheewalasaurav44@gmail.com>
2024-08-27 22:55:25 +0530

    CodeOnCouch

---
+++ CodeOnCouch/src/main/java/com/example/CodeOnCouch/CodeOnCouchApplication.java
@@ -0,0 +1,13 @@
+package com.example.CodeOnCouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeOnCouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeOnCouchApplication.class, args);
+	}
+
+}
---
+++ CodeOnCouch/src/test/java/com/example/CodeOnCouch/CodeOnCouchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodeOnCouch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeOnCouchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
